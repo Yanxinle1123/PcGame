c2a097e41114465a454c6b34695166db1838caaf
yanxinle <1020121123@qq.com>
2024-02-14 19:58:57 +0800

    实现red_line

--- pc_game/red_line.py
+++ pc_game/red_line.py
@@ -1,3 +1,8 @@
 class RedLine:
     def __init__(self):
-        pass
+        self.window_height = None
+        self.window_width = None
+        self.red_line_height = None
+        self.red_line_x0 = None
+        self.red_line_y0 = None
+        self.red_line_x1 = None

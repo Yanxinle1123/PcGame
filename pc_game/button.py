a382c6f3ad6edd986cee5eeecf771840007c336b
yanxinle <1020121123@qq.com>
2024-02-15 13:53:34 +0800

    实现button进行中

--- pc_game/button.py
+++ pc_game/button.py
@@ -1,8 +1,26 @@
-
-
 class Button:
-    def __init__(self):
-        pass
+    def __init__(self, canvas):
+        self._canvas = canvas
+
+        self._start_button = None
+        self._start_width = None
+        self._start_height = None
+
+        self._close_button = None
+        self._close_width = None
+        self._close_height = None
+
+        self._paused_button = None
+        self._paused_width = None
+        self._paused_height = None
+
+        self._continue_button = None
+        self._continue_width = None
+        self._continue_height = None
+
+        self._set_up_button = None
+        self._set_up_width = None
+        self._set_up_height = None
 
     def _get_x(self, ):
         # TODO : Complete method content

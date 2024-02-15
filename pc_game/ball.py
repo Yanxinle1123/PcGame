34cf8fe433b0a39c58ad0efefbc77ceb2e27d235
yanxinle <1020121123@qq.com>
2024-02-15 09:21:35 +0800

    实现ball进行中

--- pc_game/ball.py
+++ pc_game/ball.py
@@ -1,16 +1,19 @@
+from comm.comm_draw import ball_first
 
 
 class Ball:
-    def __init__(self):
-        pass
+    def __init__(self, canvas, red_line_x1, red_line_y1, grade_map, ):
+        self._canvas = canvas
+        self._red_line_x1 = red_line_x1
+        self._red_line_y1 = red_line_y1
+        self._grade_map = grade_map
+        self._ball_x1 = self._red_line_x1 // 2
+        self._ball_y1 = self._red_line_y1 - 45
+        self._ball_x1 = self._ball_x1 - 30 / 2
+        self._ball = None
 
-    def _get_x(self, ):
-        # TODO : Complete method content
-        pass
-
-    def _get_y(self, ):
-        # TODO : Complete method content
-        pass
+    def draw_ball(self):
+        self._ball = ball_first(self._canvas, self._grade_map["ball_color"], self._ball_x1, self._ball_y1)
 
     def _get_color(self, ):
         # TODO : Complete method content

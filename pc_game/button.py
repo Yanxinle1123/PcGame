409bfb632f6b5ea403100d5b70fb389be36f3ec2
yanxinle <1020121123@qq.com>
2024-02-15 14:08:20 +0800

    实现button进行中

--- pc_game/button.py
+++ pc_game/button.py
@@ -2,52 +2,64 @@ from tkinter import Button
 
 
 class PcButton:
-    def __init__(self, canvas):
+    def __init__(self, canvas, button_text_size, continue_button_text_color, pause_button_text_color):
         self._canvas = canvas
+        self._button_text_size = button_text_size
 
         self._start_button = None
         self._start_width = None
         self._start_height = None
+        self._start_command = None
 
         self._close_button = None
         self._close_width = None
         self._close_height = None
+        self._close_command = None
 
         self._paused_button = None
         self._paused_width = None
         self._paused_height = None
+        self._paused_command = None
+        self._pause_button_text_color = pause_button_text_color
 
         self._continue_button = None
         self._continue_width = None
         self._continue_height = None
+        self._continue_command = None
+        self._continue_button_text_color = continue_button_text_color
 
         self._set_up_button = None
         self._set_up_width = None
         self._set_up_height = None
+        self._set_up_command = None
 
     def _draw_button(self):
-        self._start_button = Button(self._canvas, text='开始', font=("Arial", button_text_size), fg='black',
-                                    command=start_game)
+        self._start_button = Button(self._canvas, text='开始', font=("Arial", self._button_text_size), fg='black',
+                                    command=self._start_command)
         self._start_width = self._start_button.winfo_reqwidth()
         self._start_height = self._start_button.winfo_reqheight()
 
-        close_button = Button(canvas, text='退出', font=("Arial", button_text_size), command=close_game)
-        close_width = close_button.winfo_reqwidth()
-        close_height = close_button.winfo_reqheight()
+        self._close_button = Button(self._canvas, text='退出', font=("Arial", self._button_text_size),
+                                    command=self._close_command)
+        self._close_width = self._close_button.winfo_reqwidth()
+        self._close_height = self._close_button.winfo_reqheight()
 
-        pause_button = Button(canvas, text='暂停', font=("Arial", button_text_size), fg=pause_button_text_color,
-                              command=pause_game)
-        pause_width = pause_button.winfo_reqwidth()
-        pause_height = pause_button.winfo_reqheight()
+        self._pause_button = Button(self._canvas, text='暂停', font=("Arial", self._button_text_size),
+                                    fg=self._pause_button_text_color,
+                                    command=self._paused_command)
+        self._pause_width = self._pause_button.winfo_reqwidth()
+        self._pause_height = self._pause_button.winfo_reqheight()
 
-        continue_button = Button(canvas, text='继续', font=("Arial", button_text_size), fg=continue_button_text_color,
-                                 command=continue_game)
-        continue_width = continue_button.winfo_reqwidth()
-        continue_height = continue_button.winfo_reqheight()
+        self._continue_button = Button(self._canvas, text='继续', font=("Arial", self._button_text_size),
+                                       fg=self._continue_button_text_color,
+                                       command=self._continue_command)
+        self._continue_width = self._continue_button.winfo_reqwidth()
+        self._continue_height = self._continue_button.winfo_reqheight()
 
-        set_up_button = Button(canvas, text='设置', font=("Arial", button_text_size), command=set_up)
-        set_up_width = continue_button.winfo_reqwidth()
-        set_up_height = continue_button.winfo_reqheight()
+        self._set_up_button = Button(self._canvas, text='设置', font=("Arial", self._button_text_size),
+                                     command=self._set_up_command)
+        self._set_up_width = self._continue_button.winfo_reqwidth()
+        self._set_up_height = self._continue_button.winfo_reqheight()
 
     def _get_x(self, ):
         # TODO : Complete method content

dd5d2ca2a187bfe8d319b324053e3f876b0baa97
yanxinle <1020121123@qq.com>
2024-02-15 10:08:37 +0800

    实现ball进行中

--- pc_game/game_window.py
+++ pc_game/game_window.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 from LeleEasyTkinter.easy_auto_window import EasyWindow
 
+from pc_game.ball import Ball
 from pc_game.grade import Grade
 from pc_game.music import Music
 from pc_game.red_line import RedLine
@@ -20,6 +21,82 @@ class GameWindow:
         self._music = Music()
         self._buttons = []
         self._game_chars = []
+        self._yellow = 'green'
+        self._letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                         'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+        self._rainbow = f'red,blue,{self._yellow}'
+        self._two_color = "red,blue"
+        self._button_color = 'lower'
+
+        self._lower_list = [
+            {"move_char_time_ms": 20, "other_char": 0, "gen_char_time_ms": 500,
+             "ball_color": "red", "char_color": "red"},  # 第 1 关
+            {"move_char_time_ms": 15, "other_char": 0, "gen_char_time_ms": 500,
+             "ball_color": "red", "char_color": "red"},  # 第 2 关
+            {"move_char_time_ms": 10, "other_char": 0, "gen_char_time_ms": 600,
+             "ball_color": "blue", "char_color": "blue"},  # 第 3 关
+            {"move_char_time_ms": 20, "other_char": 2, "gen_char_time_ms": 400,
+             "ball_color": "green", "char_color": self._yellow},  # 第 4 关
+            {"move_char_time_ms": 20, "other_char": 2, "gen_char_time_ms": 300,
+             "ball_color": "red", "char_color": self._two_color},  # 第 5 关
+            {"move_char_time_ms": 20, "other_char": 5, "gen_char_time_ms": 200,
+             "ball_color": "red", "char_color": self._rainbow},  # 第 6 关
+            {"move_char_time_ms": 10, "other_char": 3, "gen_char_time_ms": 200,
+             "ball_color": self._yellow, "char_color": f"{self._yellow},red"},  # 第 7 关
+            {"move_char_time_ms": 20, "other_char": 3, "gen_char_time_ms": 400,
+             "ball_color": "blue", "char_color": self._rainbow},  # 第 8 关
+            {"move_char_time_ms": 15, "other_char": 5, "gen_char_time_ms": 300,
+             "ball_color": self._yellow, "char_color": self._rainbow},  # 第 9 关
+            {"move_char_time_ms": 10, "other_char": 8, "gen_char_time_ms": 250,
+             "ball_color": "red", "char_color": self._rainbow},  # 第 10 关
+        ]
+
+        self._medium_list = [
+            {"move_char_time_ms": 15, "other_char": 2, "gen_char_time_ms": 400,
+             "ball_color": "red", "char_color": "red"},  # 第 1 关
+            {"move_char_time_ms": 10, "other_char": 2, "gen_char_time_ms": 500,
+             "ball_color": "red", "char_color": "red"},  # 第 2 关
+            {"move_char_time_ms": 10, "other_char": 2, "gen_char_time_ms": 500,
+             "ball_color": "blue", "char_color": "blue"},  # 第 3 关
+            {"move_char_time_ms": 15, "other_char": 2, "gen_char_time_ms": 400,
+             "ball_color": "green", "char_color": self._yellow},  # 第 4 关
+            {"move_char_time_ms": 15, "other_char": 2, "gen_char_time_ms": 300,
+             "ball_color": "red", "char_color": self._two_color},  # 第 5 关
+            {"move_char_time_ms": 15, "other_char": 5, "gen_char_time_ms": 200,
+             "ball_color": "red", "char_color": self._rainbow},  # 第 6 关
+            {"move_char_time_ms": 10, "other_char": 3, "gen_char_time_ms": 200,
+             "ball_color": self._yellow, "char_color": f"{self._yellow},red"},  # 第 7 关
+            {"move_char_time_ms": 15, "other_char": 3, "gen_char_time_ms": 400,
+             "ball_color": "blue", "char_color": self._rainbow},  # 第 8 关
+            {"move_char_time_ms": 10, "other_char": 5, "gen_char_time_ms": 300,
+             "ball_color": self._yellow, "char_color": self._rainbow},  # 第 9 关
+            {"move_char_time_ms": 10, "other_char": 8, "gen_char_time_ms": 250,
+             "ball_color": "red", "char_color": self._rainbow},  # 第 10 关
+        ]
+
+        self._advanced_list = [
+            {"move_char_time_ms": 15, "other_char": 5, "gen_char_time_ms": 400,
+             "ball_color": "red", "char_color": "red"},  # 第 1 关
+            {"move_char_time_ms": 10, "other_char": 5, "gen_char_time_ms": 500,
+             "ball_color": "red", "char_color": "red"},  # 第 2 关
+            {"move_char_time_ms": 10, "other_char": 5, "gen_char_time_ms": 500,
+             "ball_color": "blue", "char_color": "blue"},  # 第 3 关
+            {"move_char_time_ms": 15, "other_char": 7, "gen_char_time_ms": 400,
+             "ball_color": "green", "char_color": self._yellow},  # 第 4 关
+            {"move_char_time_ms": 15, "other_char": 7, "gen_char_time_ms": 300,
+             "ball_color": "red", "char_color": self._two_color},  # 第 5 关
+            {"move_char_time_ms": 15, "other_char": 10, "gen_char_time_ms": 200,
+             "ball_color": "red", "char_color": self._rainbow},  # 第 6 关
+            {"move_char_time_ms": 10, "other_char": 8, "gen_char_time_ms": 200,
+             "ball_color": self._yellow, "char_color": f"{self._yellow},red"},  # 第 7 关
+            {"move_char_time_ms": 15, "other_char": 8, "gen_char_time_ms": 400,
+             "ball_color": "blue", "char_color": self._rainbow},  # 第 8 关
+            {"move_char_time_ms": 10, "other_char": 10, "gen_char_time_ms": 300,
+             "ball_color": self._yellow, "char_color": self._rainbow},  # 第 9 关
+            {"move_char_time_ms": 10, "other_char": 8, "gen_char_time_ms": 200,
+             "ball_color": "red", "char_color": self._rainbow},  # 第 10 关
+        ]
 
     def _create_grade(self, ):
         # TODO : Complete method content
@@ -62,4 +139,5 @@ class GameWindow:
         self._canvas.pack()
         red_line = RedLine(self._window, self._canvas, easy_window.get_window_height(), easy_window.get_window_width())
         red_line.draw_red_lines()
+        ball = Ball(self._canvas, red_line.get_red_line_x1(), red_line.get_red_line_y1(), )
         self._window.mainloop()
--- pc_game/red_line.py
+++ pc_game/red_line.py
@@ -15,3 +15,15 @@ class RedLine:
         self._red_line = self._canvas.create_line(self._red_line_x0, self._red_line_y0, self._red_line_x1,
                                                   self._red_line_y1, fill='red',
                                                   width=self._red_line_height)
+
+    def get_red_line_x0(self):
+        return self._red_line_x0
+
+    def get_red_line_y0(self):
+        return self._red_line_y0
+
+    def get_red_line_x1(self):
+        return self._red_line_x1
+
+    def get_red_line_y1(self):
+        return self._red_line_y1
